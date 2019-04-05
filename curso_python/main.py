fa24e7565ae13719c2be629cf1a4688d2e1324e4
andersonb9 <jandersonb9@gmail.com>
2019-04-04 20:38:29 -0500

    Phyton
    primer git

---
+++ curso_python/main.py
@@ -0,0 +1,34 @@
+""" print("Hello World")
+#Entrada de dtos 
+x = float (input("Ingrese datos: "))
+#print(x)
+y = float (input("Ingrese datos: "))
+print(10%2)
+a = True
+b = False
+print(a) """
+
+
+""" a = 3
+if a<4:
+    print("Hola")
+    print("Mundo")
+elif a<5:
+    print("es menor que 5")
+else:
+    print("No es menor") """
+
+a = 3
+b = 4
+if a == 3 and b == 4:
+    print("Es correcto ")
+
+""" 
+a = 7
+while a==7:
+    print("Estoy en el while ")
+    a = int(input("Ingrese el valor de "))
+    if a != 7:
+        break """
+
+       
